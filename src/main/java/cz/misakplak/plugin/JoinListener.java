87230c91cfffd4d6eee05090ee8a65d3f5ae7e9d
misakplak <misakplakk@gmail.com>
2026-06-12 22:59:03 +0200

    some fixes

--- src/main/java/cz/misakplak/plugin/JoinListener.java
+++ src/main/java/cz/misakplak/plugin/JoinListener.java
@@ -1,37 +1,39 @@
 package cz.misakplak.plugin;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.EventPriority;
 
 
 public class JoinListener implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
 
     public void onJoin(PlayerJoinEvent event) {
 
+        event.setJoinMessage(null);
+
+
         Player player = event.getPlayer();
 
-        player.sendMessage(
+        event.getPlayer().sendMessage(
                 PaperPlugin.getInstance()
                         .getConfig()
                         .getString("join-message")
         );
     }
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onLeave(PlayerQuitEvent event) {
 
-        Player player = event.getPlayer();
-        Bukkit.broadcastMessage(player.getName(
-        ) +
+        event.setQuitMessage(null);
+
+        event.getPlayer().sendMessage(
                 PaperPlugin.getInstance()
                 .getConfig()
                 .getString("leave-message")
                 );
-
     }
 }
